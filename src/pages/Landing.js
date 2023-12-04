345d82f32346e47e4bd823bbd01179569a9b2e32
engmhajj <144273476+engmhajj@users.noreply.github.com>
2023-12-05 01:20:34 +0200

    changed main paragraph

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -13,11 +13,7 @@ const Landing = () => {
           <h1>
             Job <span>Tracking</span> App
           </h1>
-          <p>
-            Crucifix narwhal street art asymmetrical, humblebrag tote bag pop-up
-            fixie raclette taxidermy craft beer. Brunch bitters synth, VHS
-            crucifix heirloom meggings bicycle rights.
-          </p>
+          <p>Let us Manage the job for you!</p>
           <Link to="/login" className="btn btn-hero">
             Login/Register
           </Link>
