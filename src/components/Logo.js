6ffc56433bac6a82e1317acab79bce3e21e19aeb
engmhajj <144273476+engmhajj@users.noreply.github.com>
2023-12-06 15:50:13 +0200

    logo remove

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,5 +1,3 @@
-import logo from "../assets/images/logo.svg";
-
 const Logo = () => {
   // return <img src={logo} alt="jobster logo" className="logo" />;
   return <div className="main-logo logo">Mhh</div>;
